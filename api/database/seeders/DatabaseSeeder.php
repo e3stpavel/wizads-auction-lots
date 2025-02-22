a36d5ce74dbb60a7972ac2a6488b644ea2ef23ce
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2025-02-22 11:58:51 +0200

    fix: fix docker images [skip ci]

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\App;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,14 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
+        if (App::environment('local')) {
+            // User::factory(10)->create();
+
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+            return;
+        }
+
+        User::create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
         ]);
 
-        //        $this->call(LocationSeeder::class);
         $this->call(LotSeeder::class);
     }
 }
